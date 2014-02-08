31d1194c8084f3eecea93bc8d91819838e0c2e7d
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2014-02-08 17:14:02 -0500

    Change code style

--- bin/load-data.pl
+++ bin/load-data.pl
@@ -12,43 +12,43 @@ use DBI;
 main(@ARGV);
 
 sub main {
-    my ( $help, $verbose, $debug, $libraryFile, $databaseFile );
+    my ( $help, $verbose, $debug, $library_file, $database_file );
 
     GetOptionsFromArray(
         \@_,
         'help|h'      => \$help,
         'verbose|v'   => \$verbose,
         'debug|d'     => \$debug,
-        'library|l=s' => \$libraryFile,
-        'database=s'  => \$databaseFile,
+        'library|l=s' => \$library_file,
+        'database=s'  => \$database_file,
     ) or pod2usage();
 
     pod2usage( -exitval => 0, -verbose => 2 ) if $help;
 
-    defined $libraryFile or die "No input file specified\n";
+    defined $library_file or die "No input file specified\n";
 
-    -f $libraryFile or die "Not a file '$libraryFile'\n";
+    -f $library_file or die "Not a file '$library_file'\n";
 
-    my $library = Mac::iTunes::Library::XML->parse($libraryFile);
+    my $library = Mac::iTunes::Library::XML->parse($library_file);
 
-    my $dbh = DBI->connect( 'dbi:SQLite:dbname=' . $databaseFile );
+    my $dbh = DBI->connect( 'dbi:SQLite:dbname=' . $database_file );
     my $sql = 'UPDATE songs SET rating = ? WHERE artist = ? and title = ?';
     my $sth = $dbh->prepare($sql);
 
     say 'Track count: ' . $library->num();
 
     my %items = $library->items();
-    while ( my ( $artistName, $songs ) = each %items ) {
-        say 'Artist: ' . $artistName;
-        while ( my ( $songName, $songItems ) = each %$songs ) {
-            say 'Song: ' . $songName;
+    while ( my ( $artist_name, $songs ) = each %items ) {
+        say 'Artist: ' . $artist_name;
+        while ( my ( $song_name, $song_items ) = each %$songs ) {
+            say 'Song: ' . $song_name;
             my $rating;
-            for my $item (@$songItems) {
+            for my $item (@$song_items) {
                 $rating = $item->rating() // 0;
                 say 'Rating: ' . $rating;
             }
 
-            $sth->execute( $rating / 100, $artistName, $songName );
+            $sth->execute( $rating / 100, $artist_name, $song_name );
         }
         say '-' x 76;
     }
